cb256b270eb2a0752a06490fa5fd3727191ad9a9
Maciej Gumieniak <maciejgumieniak007@gmail.com>
2019-01-12 11:32:15 +0100

    Init commit

---
+++ src/main/java/com/jdbc/h2/jdbc_h2/JdbcH2Application.java
@@ -0,0 +1,14 @@
+package com.jdbc.h2.jdbc_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcH2Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/jdbc/h2/jdbc_h2/JdbcH2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jdbc.h2.jdbc_h2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbcH2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
